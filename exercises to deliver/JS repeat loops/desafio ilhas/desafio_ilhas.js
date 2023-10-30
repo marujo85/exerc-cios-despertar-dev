2a5775b95c98845d7ed503650b9f298f57a8baf4
Mirna Araújo <mirnadocris@gmail.com>
2023-10-30 16:04:28 -0300

    detalhes desafio

--- exercises to deliver/JS repeat loops/desafio ilhas/desafio_ilhas.js
+++ exercises to deliver/JS repeat loops/desafio ilhas/desafio_ilhas.js
@@ -1,28 +1,30 @@
 let iniciar = confirm("Deseja realizar um saque?")
+
 while (iniciar){
 
-const saque = parseInt(prompt("Insira o valor do saque"))
+	const saque = parseInt(prompt("Insira o valor do saque"))
 
-let resto
+	let resto
 
-let nota50 = parseInt(saque/50)
-resto = saque % 50
+	let nota50 = parseInt(saque/50)
+	resto = saque % 50
 
-let notas10 = parseInt(resto/10)
-resto = resto % 10
+	let notas10 = parseInt(resto/10)
+	resto = resto % 10
 
-let notas5 = parseInt(resto/5)
-resto = resto % 5
+	let notas5 = parseInt(resto/5)
+	resto = resto % 5
 
-let notas1 = resto
+	let notas1 = resto
 
-console.log(`GC$ 50,00 -> ${nota50}
-GC$ 10,00 -> ${notas10}
-GC$ 5,00 -> ${notas5}
-GC$ 1,00 ->  ${notas1}`)
 
-document.write(`Resultado do saque de R$${saque}`)
-document.write (`${resultado}`)
+
+document.write(`Resultado do saque de R$${saque} </br>`)
+document.write (`GC$ 50,00 -> ${nota50} </br>
+GC$ 10,00 -> ${notas10} </br>
+GC$ 5,00 -> ${notas5} </br>
+GC$ 1,00 ->  ${notas1} </br>`)
 
 iniciar = confirm(`Deseja realizar mais um saque?`)
+
 }
